6c0a9121e3f55adb7683d4b5d4dd055ab2233411
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-14 14:40:26 -0700

    done

--- client/index.js
+++ client/index.js
@@ -1,2 +1,49 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+  $('#getRandomNums').click(get20Nums);
+  $('#evens').on('click', '.evenNum', squareRoot);
+}
+
+function squareRoot(){
+  var evenNum = $(this).text();
+  $(this).text(Math.sqrt(evenNum).toFixed(2));
+}
+
+function get20Nums(){
+  var nums = 20;
+  var url = 'https://qrng.anu.edu.au/API/jsonI.php?length=20&type=uint16';
+  $.getJSON(url, function(response){
+    var threes = [];
+    var others = [];
+    $('#evens').children().remove();
+    $('#threes').children().remove();
+    $('#others').children().remove();
+    response.data.forEach(function(num){
+      var $div = $('<div>');
+      if (num % 2 === 0){
+        $div.text(num);
+        $div.addClass('evenNum')
+        $('#evens').append($div);
+      }
+      else if (num % 3 === 0) {
+        threes.push(num);
+      }
+      else {
+        others.push(num);
+      }
+    });
+    var $div3 = $('<div>');
+    var $othersdiv = $('<div>');
+    $div3.text(threes.join(', '));
+    $othersdiv.text(others.join(', '));
+    $('#threes').append($div3);
+    $('#others').append($othersdiv);
+
+    // $div.addClass('quote');
+    // $div.text('$' + response.LastPrice);
+    // $('#quotes').append($div);
+  });
+}
